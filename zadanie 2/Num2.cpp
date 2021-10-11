6a84ca2789b0c3e68c66bb3844a565725ea01f00
RailGiz <91553116+RailGiz@users.noreply.github.com>
2021-10-11 20:31:37 +0300

    Update and rename dz.cpp to Num2.cpp

---
+++ zadanie 2/Num2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int fib(int number)
+{
+	if (number < 1)
+	{
+		return 0;
+	}
+	if (number == 1)
+	{
+		return 1;
+	}
+
+	return fib(number - 1) + fib(number - 2);
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Вас приветствует программа, которая вычисляет N-ое число Фибоначчи" << endl;
+	int number;
+	cout << "Введите число: " << endl;
+	cin >> number;
+	cout << number << " по счету число Фибоначчи равняется: " << fib(number) << endl;
+}
