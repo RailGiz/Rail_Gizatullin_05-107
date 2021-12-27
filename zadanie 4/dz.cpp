ae4329bfce284e8252d9f4965bbba64e8a2fdb61
RailGiz <91553116+RailGiz@users.noreply.github.com>
2021-12-27 21:03:45 +0300

    Update dz.cpp

--- zadanie 4/dz.cpp
+++ zadanie 4/dz.cpp
@@ -20,12 +20,18 @@ void rand_per(int* arr, const int size)
 	for (int i = 0; i < size; i++)
 	{
 		arr[i] = 1 + rand() % size;
-		for (int o=0;o<i;o++)
+		for (int o = 0; o < i; o++)
 			if (arr[o] == arr[i])
 			{
 				i--;
 			}
 	}
+}
+
+void rnd_arr(int* arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+		*(arr++) = -1000 + rand() % 2001;
 }
 
 int main()
@@ -35,9 +41,8 @@ int main()
 	const int size = 10;
 	int* arr = new int[size];
 
-
 	int a, d;
-	cout << "Введите первый элемент арифм прогрессии: " << endl;
+	cout << "Введите первый элемент арифметической прогрессии: " << endl;
 	cin >> a;
 	cout << "Введите разность: " << endl;
 	cin >> d;
@@ -48,4 +53,24 @@ int main()
 	cout << "Случайная перестановка: ";
 	rand_per(arr, size);
 	print_array(arr, size);
+
+	rnd_arr(arr, size);
+	cout << endl << "Случайные числа: ";
+	print_array(arr, size);
+
+	int even_count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		int ab = arr[i] % 100, b = arr[i] % 10;
+
+		if (ab - b == 20)
+			even_count++;
+	}
+
+	cout << "Количество чисел с предпоследней цифрой 2: ";
+	cout << even_count;
+
+	cout << endl;
+	return 0;
 }
