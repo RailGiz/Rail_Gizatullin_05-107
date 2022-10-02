713243476e56f0321ab2d2bcddf2dac7a145bbb6
RailGiz <91553116+RailGiz@users.noreply.github.com>
2022-10-02 19:29:04 +0300

    Create Program.cs

---
+++ Dz2/1)Rasstmt/Program.cs
@@ -0,0 +1,16 @@
+Point3D Read3DPoint()
+{
+    Console.Write("Введите координаты x: ");
+    var x = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите координаты y: ");
+    var y = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите координаты z: ");
+    var z = Convert.ToDouble(Console.ReadLine());
+    return new Point3D(x, y, z);
+}
+Console.WriteLine("Введите первую точку:");
+var p1 = Read3DPoint();
+Console.WriteLine("Введите вторую точку:");
+var p2 = Read3DPoint();
+var distance = GeomertyMath.Distance(p1, p2);
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
