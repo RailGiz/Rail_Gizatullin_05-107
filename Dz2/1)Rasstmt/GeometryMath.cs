73ad0a12fc96c67d057eb310c40edb8918cf49ff
RailGiz <91553116+RailGiz@users.noreply.github.com>
2022-10-02 20:36:33 +0300

    Update GeometryMath.cs

--- Dz2/1)Rasstmt/GeometryMath.cs
+++ Dz2/1)Rasstmt/GeometryMath.cs
@@ -9,4 +9,10 @@ public static class GeomertyMath
     {
        return Math.Sqrt(Math.Pow(0 - p1.X, 2) + Math.Pow(0 - p1.Y, 2) + Math.Pow(0 - p1.Z, 2));
     }
+
+    public static double Scalar(Point3D p1, Point3D p2)
+    {
+        return ((p1.X * p2.X) + (p1.Y * p2.Y) + (p1.Z * p2.Z));
+    }
+
 }
