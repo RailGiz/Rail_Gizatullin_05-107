7bfb0468bb7a5c415d138960c580ac6aef217b0c
RailGiz <91553116+RailGiz@users.noreply.github.com>
2022-10-02 19:29:53 +0300

    Create GeometryMath.cs

---
+++ Dz2/1)Rasstmt/GeometryMath.cs
@@ -0,0 +1,7 @@
+public static class GeomertyMath
+{
+    public static double Distance(Point3D p1, Point3D p2)
+    {
+        return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y) + (p1.Z - p2.Z) * (p1.Z - p2.Z));
+    }
+}
