0b069d3f4d65be0bb8adedc9eb4401a8b02ed8d4
RailGiz <91553116+RailGiz@users.noreply.github.com>
2022-10-02 20:59:29 +0300

    Create Summa.cs

---
+++ Dz2/1)Rasstmt/Summa.cs
@@ -0,0 +1,16 @@
+public static class Summa
+{
+    public static double Coord1(Point3D p1, Point3D p2)
+    {
+        return ((p1.X + p2.X));
+    }
+    public static double Coord2(Point3D p1, Point3D p2)
+    {
+        return ((p1.Y + p2.Y));
+    }
+    public static double Coord3(Point3D p1, Point3D p2)
+    {
+        return ((p1.Z + p2.Z));
+    }
+
+}
