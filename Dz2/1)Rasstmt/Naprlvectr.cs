b5060f9d444cb90c84fea1a043496c208f1b4e24
RailGiz <91553116+RailGiz@users.noreply.github.com>
2022-10-02 21:17:14 +0300

    Create Naprlvectr.cs

---
+++ Dz2/1)Rasstmt/Naprlvectr.cs
@@ -0,0 +1,16 @@
+public static class Naprlvectr
+{
+    public static double Coord1(Point3D p1, Point3D p2)
+    {
+        return ((p2.X - p1.X));
+    }
+    public static double Coord2(Point3D p1, Point3D p2)
+    {
+        return ((p2.Y - p1.Y));
+    }
+    public static double Coord3(Point3D p1, Point3D p2)
+    {
+        return ((p2.Z - p1.Z));
+    }
+
+}
